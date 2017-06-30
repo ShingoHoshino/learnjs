4a8aac0f114bc9d287b12f5849017be011b101d0
Hoshino, Shingo <hoshino.shingo@gmail.com>
2017-06-30 17:18:35 +0900

    3章の実施

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -31,9 +31,35 @@ describe('LearnJS', function() {
 });
 
 describe('problem view', function(problemNumber) {
-  it('has a title that includes the problem number', function() {
-    var view = learnjs.problemView('1');
-    expect(view.text()).toEqual('Problem #1 Coming soon!');
+  var view;
+  beforeEach(function() {
+    view = learnjs.problemView('1');
   });
-});
 
+  it('has a title that includes the problem number', function() {
+    expect(view.find('.title').text()).toEqual('Problem #1');
+  });
+
+  it('shows the description', function() {
+    expect(view.find('[data-name="description"]').text()).toEqual('What is truth?');
+  });
+
+  it('shows the problem code', function() {
+    expect(view.find('[data-name="code"]').text()).toEqual('function problem() { return __; }');
+  });
+
+  describe('answer section', function() {
+    it('can check a correct answer by hitting a button', function() {
+      view.find('.answer').val('true');
+      view.find('.check-btn').click();
+      expect(view.find('.result').text()).toEqual('Correct!');
+    });
+    it('can check a incorrect answer by hitting a button', function() {
+      view.find('.answer').val('false');
+      view.find('.check-btn').click();
+      expect(view.find('.result').text()).toEqual('Incorrect!');
+    });
+
+  });
+
+});
